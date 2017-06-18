9841c8754c279544539507fd541e94fa250e6396
Muhammad Usman <musmanraodev@gmail.com>
2017-06-18 16:28:05 -0400

    added rooms

--- server.js
+++ server.js
@@ -32,32 +32,31 @@ io.on("connection", function(socket) {
       name: socket.username,
     });
   });
+
+  socket.on("user joined", function(data) {
+    socket.username = data.name;
+    io.sockets.in(data.room).emit("got new user", {
+      name: socket.username,
+    });
+  });
   /* User Ends */
 
   /* Room Starts */
   socket.on("room", function(room) {
-    if ((Object.keys(io.sockets.adapter.sids[socket.id])).length >= 2) {
-      // console.log((Object.keys(io.sockets.adapter.sids[socket.id])).length);
-      
+    if (Object.keys(io.sockets.adapter.sids[socket.id]).length >= 2) {
       socket.leave(socket.rooms[Object.keys(socket.rooms)[1]]);
-      // console.log('old room is ' + socket.rooms[Object.keys(socket.rooms)[1]]);
-      
-      // console.log('current room is' + room);
-      
     }
     socket.join(room);
-    console.log(
-      "current rooms are: " + Object.keys(io.sockets.adapter.sids[socket.id])
-    );
+    console.log("Listing All Rooms: " + io.sockets.adapter.rooms);
+    io.sockets.emit("rooms count", io.sockets.adapter.rooms);
   });
-
   /* Room Ends */
 
   /* Chat Section Starts in Server */
   socket.on("chat message", function(data) {
-    console.log('this is the current room for this user=> ' + data.room);
+    console.log("this is the current room for this user=> " + data.room);
     console.log(data.msg);
-    
+
     io.sockets.in(data.room).emit("new message", {
       msg: data.msg,
       name: socket.username,
@@ -69,43 +68,36 @@ io.on("connection", function(socket) {
     // });
   });
 
-  socket.on("user joined", function(name) {
-    socket.username = name;
-    io.emit("got new user", {
-      name: socket.username,
-    });
-  });
-
   /* Chat Ends in Server */
 
   /* Youtube Section Starts in Server */
 
   socket.on("sending url to server", function(data) {
-    io.sockets.in(data.room).emit('sending url to everyone', {
-      url: data.url,  
+    io.sockets.in(data.room).emit("sending url to everyone", {
+      url: data.url,
     });
   });
 
   socket.on("play video now to server", function(room) {
-    io.sockets.in(room).emit('playing video for everyone');  
+    io.sockets.in(room).emit("playing video for everyone");
   });
 
   socket.on("pause video now to server", function(room) {
-    io.sockets.in(room).emit('pausing video for everyone');
+    io.sockets.in(room).emit("pausing video for everyone");
   });
 
   socket.on("new time send to server", function(data) {
     console.log(`the following: room=>${data.room} time=>${data.time}`);
-    
-    io.sockets.in(data.room).emit('send this time to everyone', {
-      time: data.time,  
+
+    io.sockets.in(data.room).emit("send this time to everyone", {
+      time: data.time,
     });
   });
   /* Youtube Ends in Server */
 });
 
-var nsp = io.of("/abc");
-nsp.on("connection", function(socket) {
-  console.log("someone connected");
-});
-nsp.emit("hi", "everyone!");
+// var nsp = io.of("/abc");
+// nsp.on("connection", function(socket) {
+//   console.log("someone connected");
+// });
+// nsp.emit("hi", "everyone!");
